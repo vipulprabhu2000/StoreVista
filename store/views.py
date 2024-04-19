c1f1be7d74e599976599a0b0a8c2e9f660be1e41
Vipul Prabhu Shirodkar <vipulprabhu2000@gmail.com>
2024-04-19 10:51:46 +0530

    Added a about page along with seperating the previous index page into segments using base.html

--- store/views.py
+++ store/views.py
@@ -8,3 +8,6 @@ def index(request):
     products=Product.objects.all()
 
     return render(request,"index.html",{'products':products})
+
+def about(request):
+    return render(request,"about.html",{})
