5ad8f0441c0a58fcf2bad0f3619c8d83a4b93881
Vipul Prabhu Shirodkar <vipulprabhu2000@gmail.com>
2024-04-19 12:23:08 +0530

    Logging and logout functionality added .

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('',views.index,name="index"),
     path('about/',views.about,name="about"),   
+    path('login/',views.login_user,name="login"),   
+    path('logout/',views.logout_user,name="logout"),   
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Product
-
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 # Create your views here.
 
 
@@ -11,3 +12,26 @@ def index(request):
 
 def about(request):
     return render(request,"about.html",{})
+
+def login_user(request):
+
+    if request.method=="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+        user =authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,("You have succcesfully Logged in"))
+            return redirect("index")
+        else:
+            messages.success(request,"Login Unsuccessful")
+            return redirect("login")
+    else:
+        return render(request,"login.html",{})
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,("You have been logged out Successfully ....... Thank you for Stopping By..."))
+    return redirect("index")
