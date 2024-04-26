7999da5f8c612da2fa92580ef93850349ea05af9
Vipul Prabhu Shirodkar <vipulprabhu2000@gmail.com>
2024-04-26 16:03:22 +0530

    Session cookies created for cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .cart import Cart
+from store.models import Product 
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -7,7 +10,18 @@ def cart_summary(request):
     return render(request,"cart_summary.html",{})
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+
+    if request.POST.get('action')=="post":
+        product_id=int(request.POST.get('product_id'))
+        product=get_object_or_404(Product,id=product_id)
+        cart.add(product=product)
+        response=JsonResponse({'Product Name : ': product.name})
+        return response
+
+
+
+
 
 def cart_delete(request):
     pass
